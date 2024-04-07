32e1275c4db8e5c15fa52ff175d4353d7345e38b
zoelesv <zoomlee1141@gmail.com>
2024-04-07 16:56:20 -0700

    update agreement

--- backend/main.py
+++ backend/main.py
@@ -1,40 +1,49 @@
-from dotenv import load_dotenv
+import openai, os, io
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pandas as pd
 
-load_dotenv()
+app = Flask(__name__)
+CORS(app)
 
-import logging
-import os
-import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.api.routers.chat import chat_router
-from app.settings import init_settings
+api_key = os.environ.get("OPENAI_API_KEY")
 
-
-app = FastAPI()
-
-init_settings()
-
-environment = os.getenv("ENVIRONMENT", "dev")  # Default to 'development' if not set
-
-
-if environment == "dev":
-    logger = logging.getLogger("uvicorn")
-    logger.warning("Running in development mode - allowing CORS for all origins")
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+def generate_response(prompt,token):
+    openai.api_key = api_key
+    response = openai.Completion.create(
+        engine="davinci-002",
+        prompt=prompt,
+        max_tokens=token,
+        n=1,
+        stop=None,
+        temperature=0.7
     )
+    return response.choices[0].text.strip()
 
-app.include_router(chat_router, prefix="/api/chat")
+@app.route('/')
+def helookup():
+    return jsonify("HOLA EVERYONE")
+
+@app.route('/prompt', methods=['GET', 'POST'])
+def prompts():
+    if request.method == 'POST':
+        img_file = request.files['image']
+        prompt_raw = request.form.get('prompt',None)
+        subdomain =  request.form.get('subfolder')
+        filename = request.form.get('filename')
+        id = request.form.get('id').lower()
+        site = request.form.get('site')
+        article = request.form.get('article_dir').replace("\\", "/")
+        token = int(request.form.get('token', 500))
+
+        # b_name = name+"_"+id
+        prefix_path = f"{site}/input/content"
+
+        if subdomain == "None" :
+            destination_blob = os.path.join(prefix_path,filename).replace("\\", "/")
+        else :
+            destination_blob = os.path.join(prefix_path,subdomain,filename).replace("\\", "/")
 
 
 if __name__ == "__main__":
-    app_host = os.getenv("APP_HOST", "0.0.0.0")
-    app_port = int(os.getenv("APP_PORT", "8000"))
-    reload = True if environment == "dev" else False
-
-    uvicorn.run(app="main:app", host=app_host, port=app_port, reload=reload)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))        
